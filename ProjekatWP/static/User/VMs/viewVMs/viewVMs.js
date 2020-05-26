91f85e41a02a6d9c325bb309c9211eeec0ee3ae7
miloradradovic <45543741+miloradradovic@users.noreply.github.com>
2020-05-26 18:07:09 +0200

    Front za view vms za obicnog usera

--- ProjekatWP/static/User/VMs/viewVMs/viewVMs.js
+++ ProjekatWP/static/User/VMs/viewVMs/viewVMs.js
@@ -1,1 +1,105 @@
-//TODO js
+let vms = [];
+$(document).ready(function() {
+
+    $("#searchfilterdiv").hide();
+
+    $.ajax({
+        url: 'getVMs',
+        type: 'get',
+        contentType: "application/json",
+        dataType: "json",
+        success: function(data){
+            window.vms = data;
+            updateTable();
+        },
+        error: function(data){
+            window.location.href = "../../../forbidden.html";
+        }
+    })
+
+    function updateTable(){
+        $("#tableVMs tbody").children().remove();;
+        window.vms.forEach(element => {
+            $("#tableVMs tbody").append($("<tr>")
+                .click(function(){
+                    sessionStorage.setItem("vmedit", element.resourceName);
+                    window.location.href = "../editVM/editVM.html";
+                })
+                .attr("title", "Click to view the VM")
+                .attr("id", element.resourceName)
+                .append($("<td>")
+                    .text(element.resourceName))
+                .append($("<td>")
+                    .text(element.numberOfCores))
+                .append($("<td>")
+                    .text(element.RAM))
+                .append($("<td>")
+                    .text(element.GPU))
+            )
+        })
+    }
+
+    $("#showsearch").click(function(){
+        if($(this).attr("name") === "show"){
+            $(this).attr("name", "hide");
+            $(this).html("Hide search/filter form");
+            $("#searchfilterdiv").show();
+        }else{
+            $(this).attr("name", "show");
+            $(this).html("Show search/filter form");
+            $("#searchfilterdiv").hide();
+        }
+    })
+
+    $("#search_filter_button").click(function(){
+        let vmName = $("#vm_name_search_input").val();
+        let coresFrom = $("#num_of_cores_from").val();
+        if(coresFrom === ""){
+            coresFrom = -1;
+        }
+        let coresTo = $("#num_of_cores_to").val();
+        if(coresTo === ""){
+            coresTo = -1;
+        }
+        let ramFrom = $("#ram_from").val();
+        if(ramFrom === ""){
+            ramFrom = -1;
+        }
+        let ramTo = $("#ram_to").val();
+        if(ramTo === ""){
+            ramTo = -1;
+        }
+        let gpuFrom = $("#gpu_from").val();
+        if(gpuFrom === ""){
+            gpuFrom = -1;
+        }
+        let gpuTo = $("#gpu_to").val();
+        if(gpuTo === ""){
+            gpuTo = -1;
+        }
+        $.ajax({
+            url: 'searchVM',
+            type: 'post',
+            data: JSON.stringify({
+                vmName: vmName,
+                coresFrom: coresFrom,
+                coresTo : coresTo,
+                ramFrom : ramFrom,
+                ramTo : ramTo,
+                gpuFrom: gpuFrom,
+                gpuTo : gpuTo
+            }),
+            success: function(data){
+                window.vms = data;
+                updateTable();
+            },error: function(data){
+                if(data === "400 bad request"){
+                    alert("Something went wrong!");
+                }else{
+                    window.location.href = "../../../forbidden.html";
+                }
+            }
+        })
+    })
+
+})
